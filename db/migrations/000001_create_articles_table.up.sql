28938ad7599237c1172a17a6d23c77ebbfbe0d6a
Naoto Imamachi <naoto.imamachi@gmail.com>
2019-11-27 16:20:24 +0900

    Fixed migaration file for creating articles table

--- db/migrations/000001_create_articles_table.up.sql
+++ db/migrations/000001_create_articles_table.up.sql
@@ -1,10 +1,11 @@
 create table articles(
     article_id serial primary key,
     title text not null,
-    auther text not null,
+    author text not null,
     link text not null,
     description text not null,
     published varchar(10),
+    doi varchar(255) unique,
     at_created timestamp default current_timestamp,
     at_updated timestamp
 );
